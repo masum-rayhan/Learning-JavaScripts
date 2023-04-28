bb7dbabfb88460d8ce1d5644b6e63f7757ccf126
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-04-29 00:29:16 +0600

    Problem - 1 (HashTable)

--- Problem-1.js
+++ Problem-1.js
@@ -1,19 +1,49 @@
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'i'];
+
+// // 2 parameters - arrays - no size limit
+// // return true or false
+
+// function containCommonItems(arr1, arr2){
+//     for(let i=0; i < arr1.length; i++){
+//         for(let j = 0; j < arr2.length; j++){
+//             if(arr1[i] === arr2[j]){
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+
+// containCommonItems(array1, array2);
+
+// // O(a*b) - Time Complexity
+
 const array1 = ['a', 'b', 'c', 'x'];
-const array2 = ['z', 'y', 'i'];
+const array2 = ['z', 'y', 'x'];
 
-// 2 parameters - arrays - no size limit
-// return true or false
+function containCommonItems2(arr1, arr2){
+    //loop through each item of array1 and create object where properties === items in the array
+    let map = {}; 
 
-function containCommonItems(arr1, arr2){
-    for(let i=0; i < arr1.length; i++){
-        for(let j = 0; j < arr2.length; j++){
-            if(arr1[i] === arr2[j]){
-                return true;
-            }
+    for(let i = 0; i < arr1.length; i++){
+       if(!map[arr1[i]]){
+         const item = arr1[i];
+         map[item] = true;
+       } 
+    }
+
+    // loop through array2 and check if item in array2 exists on created object
+    
+    for(let j =0; j < arr2.length; j++){
+        if(map[arr2[j]]){
+           return true;   
         }
     }
     return false;
 }
 
-containCommonItems(array1, array2);
+    containCommonItems2(array1, array2);
+
+    //O(a + b) - Time Complexity
 
